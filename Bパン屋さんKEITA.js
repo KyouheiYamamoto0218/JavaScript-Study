dd59d94a485f03ea5e051621e814cdc9c95d27f3
kyouhei <kmsyam1014@gmail.com>
2023-04-08 23:29:41 +0900

    test

--- Bパン屋さんKEITA.js
+++ Bパン屋さんKEITA.js
@@ -10,6 +10,7 @@ let breadCount = items[0];
 let actionCount = items[1];
 let prices = [];
 let stocks = [];
+let aaaa = lines[1];
 
 for (let i = index; i < index + breadCount; i++) {
     let items = lines[i].split(' ').map(str => parseInt(str));
