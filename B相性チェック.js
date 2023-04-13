1f1d7f8afe8dea60f91a11b2764b1b80d2cc5f92
kyouhei <kmsyam1014@gmail.com>
2023-04-13 15:29:55 +0900

    B相性チェック

--- B相性チェック.js
+++ B相性チェック.js
@@ -1,18 +1,29 @@
-require = require("fshook")(__filename);
+// require = require("fshook")(__filename);
+//
+// let lines = require("fs")
+//     .readFileSync("/dev/stdin", "utf8")
+//     .split("\r\n");
+//
+// let namename = lines[0];
+// let name = namename.replace(" ","");
+// let nameArray = name.split("");
+//
+// let words = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26];
+//
+// for (let i = 0; i < words.length; i++) {
+//     nameArray = nameArray.replace(words[i], numbers[i]);
+// }
 
-let lines = require("fs")
-    .readFileSync("/dev/stdin", "utf8")
-    .split("\r\n");
+let testArray = [16, 1, 9, 26, 1];
+let number = testArray.length;
+let aaaArray = [];
 
-let namename = lines[0];
-let name = namename.replace(" ","");
-let nameArray = name.split("");
-
-let words = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26];
-
-for (let i = 0; i < words.length; i++) {
-    nameArray = nameArray.replace(words[i], numbers[i]);
+for (let i = 0; i < number - 1; i++) {
+    for (let j = 0; j < number; j++) {
+        let plus = testArray[j] + testArray[j + 1]
+        aaaArray.push(plus);
+    }
+    testArray = aaaArray;
 }
-
-console.log(nameArray);
+console.log(aaaArray);
