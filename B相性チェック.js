22878e71232d8f2b0bbbf737fccf84b72d31fdd9
kyouhei <kmsyam1014@gmail.com>
2023-04-15 01:45:31 +0900

    B相性チェック（完成）

--- B相性チェック.js
+++ B相性チェック.js
@@ -6,37 +6,73 @@ let lines = require("fs")
 
 let namename = lines[0];
 let name = namename.replace(" ","");
-let words = "abcdefghijklmnopqrstuvwxyz"
 
-let words2 = [];
+let words = []; //abcdefghijklmnopqrstuvwxyz
 for (let i = "a".charCodeAt(0); i <= "z".charCodeAt(0); i++) {
-    words2.push(String.fromCharCode(i));
+    words.push(String.fromCharCode(i));
 }
-console.log(words2);
-
-
 
 let object = {};    //空のオブジェクト
-for (let i = 1; i < words.length; i++) {
-    object[words[i]] = i
+for (let i = 0; i < words.length; i++) {
+    object[words[i]] = i + 1
 }
-console.log(object["c"]);
 
-let object2 = {
-    "a": 1,
-};
+let namecount = name.length;
+let testArray = [];
+for (let i = 0; i < namecount; i++) {
+    let aaa = object[name[i]];
+    testArray.push(aaa);
+}
 
-
-
-let testArray = [16, 1, 9, 26, 1];
 let number = testArray.length;
 
 for (let i = 0; i < number - 1; i++) {
     let aaaArray = [];
     for (let j = 0; j < testArray.length - 1; j++) {
         let plus = testArray[j] + testArray[j + 1];
-        aaaArray.push(plus);
+
+        if (plus > 100) {
+            plus -= 101;
+            aaaArray.push(plus);
+        } else {
+            aaaArray.push(plus);
+        }
     }
     testArray = aaaArray;
 }
-console.log(testArray);
+let patternA = (testArray[0]);
+
+//-----------------------------------------
+
+let nameArray = namename.split(" ");
+let a = nameArray[0];
+let b = nameArray[1];
+let name2 = b + a;  //izapa
+
+let testArray2 = [];
+for (let i = 0; i < namecount; i++) {
+    let aaa = object[name2[i]];
+    testArray2.push(aaa);
+}
+
+for (let i = 0; i < number - 1; i++) {
+    let aaaArray2 = [];
+    for (let j = 0; j < testArray2.length - 1; j++) {
+        let plus = testArray2[j] + testArray2[j + 1];
+
+        if (plus > 100) {
+            plus -= 101;
+            aaaArray2.push(plus);
+        } else {
+            aaaArray2.push(plus);
+        }
+    }
+    testArray2 = aaaArray2;
+}
+let patternB = (testArray2[0]);
+
+if (patternA > patternB) {
+    console.log(patternA);
+} else {
+    console.log(patternB);
+}
