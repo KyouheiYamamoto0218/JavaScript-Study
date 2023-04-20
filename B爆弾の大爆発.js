af409791b7b6f0a184e57509ce1c2ad1df8ed3d4
kyouhei <kmsyam1014@gmail.com>
2023-04-20 22:25:27 +0900

    B爆弾の大爆発

--- B爆弾の大爆発.js
+++ B爆弾の大爆発.js
@@ -20,18 +20,33 @@ for (let i = 0; i < colum; i++) {
     }
 }
 
-let bombColum = [];
-for (let i = 0; i < colum; i++) {
-    bombColum.push("･");
+let bombColumRow = [];
+for (let i = 0; i < row; i++) {
+    let bombColum = [];
+    for (let j = 0; j < colum; j++) {
+        bombColum.push("･");
+    }
+    bombColumRow.push(bombColum);
 }
+//bombNumber [ '0 0', '0 2', '2 2' ]
+for (let i = 0; i < bombNumber.length; i++) {
+    let bombCR = bombNumber[i].split(" ");  //0 , 0
+    let bombC = parseInt(bombCR[0]);
+    let bombR = parseInt(bombCR[1]);
 
-for (let i = 0; i < colum; i++) {
     for (let j = 0; j < row; j++) {
-        let bombCR = bombNumber[j].split(" ");  //0 , 0
-        let bombC = bombCR[0];
-        let bombR = bombCR[1];
+        let changeArray = bombColumRow[bombC];  //1行目の  [・,・,・,・]
+        changeArray[j] = "#";
+    }
 
-
+    for (let k = 0; k < colum; k++) {
+        let changeArray2 = bombColumRow[k];
+        let change = changeArray2[bombR];
+        change = "#";
     }
 }
 
+console.log(bombColumRow[0]);
+console.log(bombColumRow[1]);
+console.log(bombColumRow[2]);
+console.log(bombColumRow[3]);
