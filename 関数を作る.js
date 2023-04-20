839463298c2efa9c98a08a0afa9f831147e7b433
kyouhei <kmsyam1014@gmail.com>
2023-04-21 00:08:08 +0900

    test関数

--- 関数を作る.js
+++ 関数を作る.js
@@ -18,3 +18,22 @@ function add(a, b) {
     return a + b;
 }
 
+//---------------------------------------
+
+test1(1, 2, 3);
+test1(4, 5, 6);
+
+function test1(a, b, c) {
+    console.log(`${a} ${b} ${c}`);
+}
+
+//---------------------------------
+
+console.log(test3(1, 2, 3));
+function test3(a, b, c) {
+    return a + b + c;
+}
+
+//---------------------------------
+
+
