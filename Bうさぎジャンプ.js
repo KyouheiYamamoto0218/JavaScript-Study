833c599b4650748636a5c811ae61ae9a2252487b
kyouhei <kmsyam1014@gmail.com>
2023-04-25 00:00:20 +0900

    B爆弾

--- Bうさぎジャンプ.js
+++ Bうさぎジャンプ.js
@@ -2,15 +2,22 @@ require = require("fshook")(__filename);
 
 let lines = require("fs")
     .readFileSync("/dev/stdin", "utf8")
-    .split("\n");
+    .split("\r\n");
 
 let index = lines[0].split(" ").map(str => parseInt(str));
 let field = index[0];
 let rabbitCount = index[1];
 let jumpCount = index[2];
 
-for (let i = 0; i < jumpCount; i++) {
-    for (let j = 0; j < rabbitCount; j++) {
-        let rabbit = parseInt(lines[j + 1]);
-    }
+let array = [];
+for (let i = 0; i < field; i++) {
+    array.push(i + 1)
 }
+
+for (let i = 0; i < rabbitCount; i++) {
+    let rabbit = parseInt(lines[i + 1]);
+
+    array.push(array[i] + rabbit[i])
+}
+
+console.log(array);
