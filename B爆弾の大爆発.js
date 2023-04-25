b9b805f2b46f31c58cc9a5e07702feaa6944dda4
kyouhei <kmsyam1014@gmail.com>
2023-04-25 22:38:56 +0900

    B爆弾

--- B爆弾の大爆発.js
+++ B爆弾の大爆発.js
@@ -5,14 +5,14 @@ let lines = require("fs")
     .split("\n");
 
 let fieldArray = lines[0].split(" ");   // 4 4
-let colum = parseInt(fieldArray[0]);    // 4
-let row = parseInt(fieldArray[1]);  // 4
+let row = parseInt(fieldArray[0]);    // 4
+let colum = parseInt(fieldArray[1]);  // 4
 let bombNumber = [];    //0 0 , 0 2 , 2 2
 
-for (let i = 0; i < colum; i++) {
+for (let i = 0; i < row; i++) {
     let bombRow = lines[i + 1]; // #.#.
 
-    for (let j = 0; j < row; j++) {
+    for (let j = 0; j < colum; j++) {
         let bombCheck = bombRow[j];
         if (bombCheck === "#") {
             bombNumber.push(i + " " + j);
@@ -21,9 +21,9 @@ for (let i = 0; i < colum; i++) {
 }
 
 let bombColumRow = [];
-for (let i = 0; i < row; i++) {
+for (let i = 0; i < colum; i++) {
     let bombColum = [];
-    for (let j = 0; j < colum; j++) {
+    for (let j = 0; j < row; j++) {
         bombColum.push("･");
     }
     bombColumRow.push(bombColum);
@@ -35,21 +35,21 @@ for (let i = 0; i < bombNumber.length; i++) {
     let bombC = parseInt(bombCR[0]);
     let bombR = parseInt(bombCR[1]);
 
-    for (let j = 0; j < row; j++) {
+    for (let j = 0; j < colum; j++) {
         let changeArray = bombColumRow[bombC];  //1行目の  [・,・,・,・]
         changeArray[j] = "#";
     }
 
-    for (let j = 0; j < colum; j++) {
+    for (let j = 0; j < row; j++) {
         let changeArray2 = bombColumRow[j];
         changeArray2[bombR] = "#";
     }
 }
 
 let count = 0;
-for (let i = 0; i < colum; i++) {
+for (let i = 0; i < row; i++) {
     let countColum = bombColumRow[i];
-    for (let j = 0; j < row; j++) {
+    for (let j = 0; j < colum; j++) {
         if (countColum[j] === "#") {
             count += 1;
         }
