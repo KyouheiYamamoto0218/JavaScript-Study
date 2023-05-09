0765693dd8ef76ca50a46e95d617f76f2346f1a8
kyouhei <kmsyam1014@gmail.com>
2023-05-10 00:15:58 +0900

    B山頂を探せ　80%

--- Bうさぎジャンプ.js
+++ Bうさぎジャンプ.js
@@ -4,18 +4,7 @@ let lines = require("fs")
     .readFileSync("/dev/stdin", "utf8")
     .split("\r\n");
 
-let index = lines[0].split(" ").map(str => parseInt(str));
-let field = index[0];
-let rabbitCount = index[1];
-let jumpCount = index[2];
-let rabbit = lines.slice(1, lines.length).map(str => parseInt(str));
-
-let array = [];
-for (let i = 0; i < field; i++) {
-    if (rabbit.includes(i + 1)) {
-        array.push(i + 1);
-    } else {
-        array.push(0);
-    }
-}
-console.log(array);
+うさぎが2匹なら2個の配列
+1匹目が1番目の茂み
+2匹目が3番目の茂み
+[1,3]
