9287bf9e4c6dd8528646bb8be395185f517672c7
kyouhei <kmsyam1014@gmail.com>
2023-05-10 22:18:02 +0900

    運賃計算

--- B運賃計算.js
+++ B運賃計算.js
@@ -3,3 +3,15 @@ require = require("fshook")(__filename);
 let lines = require("fs")
     .readFileSync("/dev/stdin", "utf8")
     .split("\n");
+
+let index = lines[0].split(" ");
+let yyy = index[0];
+let xxx = index[1];
+let pliceList = {};
+
+for (let y = 1; y < yyy + 1; y++) {
+    for (let x = 1; x < xxx + 1; x++) {
+        pliceList.y = lines[y].split(" ");
+    }
+}
+console.log(pliceList);
