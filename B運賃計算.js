1ac09f4f2c9218154f5ed3bc842e97189c449f59
kyouhei <kmsyam1014@gmail.com>
2023-05-11 15:16:00 +0900

    AA会員ID

--- B運賃計算.js
+++ B運賃計算.js
@@ -5,13 +5,18 @@ let lines = require("fs")
     .split("\n");
 
 let index = lines[0].split(" ");
-let yyy = index[0];
-let xxx = index[1];
+let yyy = parseInt(index[0]);
+let xxx = parseInt(index[1]);
 let pliceList = {};
 
 for (let y = 1; y < yyy + 1; y++) {
     for (let x = 1; x < xxx + 1; x++) {
-        pliceList.y = lines[y].split(" ");
+        let plice = {};
+        plice.y = y;
+        plice.plice = lines[y].split(" ");
+
+        pliceList.push(plice);
     }
 }
+
 console.log(pliceList);
