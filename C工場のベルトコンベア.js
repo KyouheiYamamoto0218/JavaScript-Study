b5853e66e5e2e285443f3da54f333d93f95e2237
kyouhei <kmsyam1014@gmail.com>
2023-07-03 23:14:05 +0900

    AA会員ID

--- C工場のベルトコンベア.js
+++ C工場のベルトコンベア.js
@@ -4,39 +4,30 @@ let lines = require("fs")
     .readFileSync("/dev/stdin", "utf8")
     .split("\n");
 
-let index = lines[0].split(" ");
-let scheduleCount = parseInt(index[0]);
-
 let schedule = lines[1].split(" ");
 let aPartsArray = lines[2].split(" ");
 let bPartsArray = lines[3].split(" ");
 
-let j = 0;
-let k = 0;
-
-let ai = 0;
-let aAnswer = parseInt([3 - ai]);
-
-let bi = 0;
-let bAnswer = parseInt([3 - bi]);
-
-for (let i = 0; i < scheduleCount; i++) {
-    let check = schedule[i];
-
-    let aParts = aPartsArray[j];
-    let bParts = bPartsArray[k];
-    hitCheck(check, aParts, bParts, j, k)
-    }
-
+let aAnswer = hitCheck(schedule, aPartsArray);
+let bAnswer = hitCheck(schedule, bPartsArray);
 console.log(`${aAnswer} ${bAnswer}`);
 
-function hitCheck(check, aParts, bParts, j, k) {
-    if (check === aParts) {
-        return ai += 1;
-        return j += 1;
+//   /と**で関数の詳細メモを記入できる。
+/**
+ *
+ *
+ * @param schedule 信号のスケジュールの配列
+ * @param partsArray 部品の進む向きの配列
+ * @return 残った部品の数
+ */
+function hitCheck(schedule, partsArray) {
+    for (let i = 0; i < schedule.length; i++) {
+        let check = schedule[i];
+        let checkParts = partsArray[0];
+
+        if (check === checkParts) {
+            partsArray.shift();　　//配列から削除　配列から消す　配列減らす
+        }
     }
-    if (check === bParts) {
-        return bi += 1;
-        return k += 1;
-    }
+    return partsArray.length;
 }
